3525c8ddd5aad41ce3d744d2f26172f2d812689e
Christopher Timm <timm@effective-webwork.de>
2020-02-26 14:04:55 +0100

    add template adjustments

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -129,11 +129,11 @@ $('.tx-dlf-navigation-first a, .tx-dlf-navigation-first span')
     .text("")
     .append('<img src="../../typo3conf/ext/presentation_package/Resources/Public/Images/icon-skipleft.svg" alt="First Page">');
 
-$('.tx-dlf-navigation-prev a, .tx-dlf-navigation-prev span')
+$('.tx-dlf-navigation-back a, .tx-dlf-navigation-back span')
     .text("")
     .append('<img src="../../typo3conf/ext/presentation_package/Resources/Public/Images/icon-doubleleft.svg" alt="Back 5 Pages">');
 
-$('.tx-dlf-navigation-back a, .tx-dlf-navigation-back span')
+$('.tx-dlf-navigation-prev a, .tx-dlf-navigation-prev span')
     .text("")
     .append('<img src="../../typo3conf/ext/presentation_package/Resources/Public/Images/icon-singleleft.svg" alt="Previous Page">');
 
@@ -157,6 +157,10 @@ $('.tx-dlf-navigation-listview a, .tx-dlf-navigation-listview span')
     .text("")
     .append('<img src="../../typo3conf/ext/presentation_package/Resources/Public/Images/icon-skipright.svg" alt="Last Page">');
 
+$('.tx-dlf-navigation-listview a, .tx-dlf-navigation-listview span')
+    .text("")
+    .append('<img src="../../typo3conf/ext/presentation_package/Resources/Public/Images/icon-tool.svg" alt="Toolbox">');
+
 
 if ($('.tx-dlf-navigation-edit').length) {
     $('ul.tx-dlf-navigation').append('<li class="tx-dlf-navigation-edit">' + $('.tx-dlf-navigation-edit').html() + '</li>');
@@ -176,7 +180,10 @@ if ($('.tx-dlf-navigation-magnifier').length) {
     $('ul.tx-dlf-navigation').append('<li class="tx-dlf-navigation-magnifier" style="padding-left: 4px;"><span></span></li>');
 }
 
-
+// TOOLBOX
+$('aside ul.tx-dlf-navigation').append('<li class="tx-dlf-navigation-tools" style="margin-left:10px;"><div id="c16" class="detail-view-tools" style="display:inline-block;">' + $('.detail-view-tools').html() + '</div></li>');
+$('aside li.tx-dlf-navigation-tools label').text("").prepend('<img src="../../typo3conf/ext/presentation_package/Resources/Public/Images/icon-tool.svg" alt="Toolbox">');
+$('aside .detail-view-itemoptions .detail-view-tools').hide();
 
 
 $('.tx-dlf-navigation-edit a, .tx-dlf-navigation-edit span')
