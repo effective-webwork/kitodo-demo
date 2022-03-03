d205d767566f7495468eed71d2784ebed3448ca9
Christopher Timm <timm@effective-webwork.de>
2022-03-03 16:17:45 +0100

    Replace menu links

--- Classes/ViewHelpers/MenuHeaderSyncViewHelper.php
+++ Classes/ViewHelpers/MenuHeaderSyncViewHelper.php
@@ -10,6 +10,10 @@ class MenuHeaderSyncViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstract
 
         $content = str_replace('<script src="https://www.sub.uni-hamburg.de/typo3conf/ext/subhh_website/Resources/Public/js/sub-website.min.js?1590473433" type="text/javascript"></script>', '', $content);
 
+        // replace links
+        $content = str_replace('https://www.sub.uni-hamburg.de/impressum.html', 'https://digitalisate.sub.uni-hamburg.de/impressum.html', $content);
+        $content = str_replace('https://www.sub.uni-hamburg.de/datenschutzerklaerung.html', 'https://digitalisate.sub.uni-hamburg.de/datenschutzerklaerung.html', $content);
+
         $content = explode('<div id="content_cols" class="clear" style="background: url(fileadmin/redaktion/Startseite/architectura.jpg) no-repeat center center fixed #8D0D1B; background-size: cover;">
         </div>', $content);
 
