9ae10274a6c147fdade491f11710f6d803d9f09f
Christopher Timm <timm@effective-webwork.de>
2022-03-11 11:00:55 +0100

    Jungius js fix

--- Resources/Public/JavaScript/Jungius/Jungius.js
+++ Resources/Public/JavaScript/Jungius/Jungius.js
@@ -1214,6 +1214,21 @@ function uniqueArray(array) {
     return newArray;
 }
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
 
 
 function createPersonIndex() {
