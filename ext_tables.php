c2d1ad2547cedb95769a617330f9446126a82ec4
Christopher Timm <timm@effective-webwork.de>
2022-09-14 16:10:59 +0200

    Fix missing extension key

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,4 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Kitodo Presentation Package');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('presentation_package', 'Configuration/TypoScript', 'Kitodo Presentation Package');
