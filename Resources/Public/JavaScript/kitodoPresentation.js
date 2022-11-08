93915c79343925c226b11600799e30a78008d688
Christopher Timm <timm@effective-webwork.de>
2022-11-08 16:24:58 +0100

    Fix newspaper routing because of renaming document type

--- Resources/Public/JavaScript/kitodoPresentation.js
+++ Resources/Public/JavaScript/kitodoPresentation.js
@@ -1124,7 +1124,7 @@ function calendarSwitchViews() {
 
 function listviewNewspaperRouting() {
     $('dd.tx-dlf-type').each(function () {
-        if ($(this).text() == 'Zeitung') {
+        if ($(this).text() == 'Jahr') {
             var url = $($(this).siblings('dd.tx-dlf-title')[0]).children().prop('href').replace('detail-zeitungen', 'kalender-zeitungen');
             $(this).siblings('dd.tx-dlf-title').children().prop('href', url);
         }
