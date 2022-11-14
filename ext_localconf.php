71138e15a169be55b02a87e962d04b5f41c8356e
Christopher Timm <timm@effective-webwork.de>
2022-11-14 16:54:17 +0100

    Overwrite language files

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,10 @@ if (!defined('TYPO3_MODE')) {
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('Kitodo.PresentationPackage', 'Page');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('presentation_package','Configuration/TypoScript', 'Presentation Package');
+
+// Override language files
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/locallang_metadata.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/de.locallang_metadata.xlf';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_structure.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/locallang_structure.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:dlf/Resources/Private/Language/locallang_structure.xlf'][] = 'EXT:presentation_package/Resources/Private/Language/Overrides/de.locallang_structure.xlf';
