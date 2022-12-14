e02b6c163c5dbedcd7eb80c4172368cc6ec1303e
Christopher Timm <timm@effective-webwork.de>
2022-12-14 14:43:00 +0100

    Add require cookie consent

--- Resources/Public/JavaScript/matomo.js
+++ Resources/Public/JavaScript/matomo.js
@@ -1,6 +1,7 @@
 
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['requireCookieConsent']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
