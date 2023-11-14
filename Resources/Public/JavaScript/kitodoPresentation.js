292bf2897ac840a298886a680ffe4871aeba685a
Michael Kubina <michael.kubina@sub.uni-hamburg.de>
2023-11-14 15:20:04 +0100

    fix license text replacement in facets

--- Resources/Public/JavaScript/kitodoPresentation.js
+++ Resources/Public/JavaScript/kitodoPresentation.js
@@ -403,22 +403,23 @@ function cleanup() {
 
     // replace copyright link in facet with name
     $('span.tx-dlf-facet-value-title').each(function () {
+        var license_facet = $(this).text().trim();
         // public domain
-        if( $(this).text() == 'https://creativecommons.org/publicdomain/mark/1.0/' || 
-            $(this).text() == 'http://creativecommons.org/publicdomain/mark/1.0/'
+        if( license_facet == 'https://creativecommons.org/publicdomain/mark/1.0/' || 
+            license_facet == 'http://creativecommons.org/publicdomain/mark/1.0/'
             ) {
             $(this).text('Public Domain Mark 1.0');
         }
 
         // in copyright
-        if( $(this).text() == 'http://rightsstatements.org/vocab/InC/1.0/' ) {
+        if( license_facet == 'http://rightsstatements.org/vocab/InC/1.0/' ) {
             $(this).text('Urheberrechtsschutz 1.0');
         }
 
         // copyright not evaluated
-        if( $(this).text() == 'https://rightsstatements.org/page/CNE/1.0/' || 
-            $(this).text() == 'https://rightsstatements.org/page/CNE/1.0/?language' ||
-            $(this).text() == 'https://rightsstatements.org/page/CNE/1.0/?language%3Dde'
+        if( license_facet == 'https://rightsstatements.org/page/CNE/1.0/' || 
+            license_facet == 'https://rightsstatements.org/page/CNE/1.0/?language' ||
+            license_facet == 'https://rightsstatements.org/page/CNE/1.0/?language%3Dde'
             ) {
             $(this).text('Urheberrechtsschutz nicht bewertet');
         }
