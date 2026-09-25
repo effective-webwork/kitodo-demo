958858f67d698cee26e50aa55a04d71a81ca9bb0
Christopher Timm <timm@effective-webwork.de>
2026-09-25 11:58:35 +0200

    Override language file

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,12 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Kitodo\Dlf\Hooks\ItemsProcFunc::c
     'className' =>  \Kitodo\PresentationPackage\Hooks\ItemsProcFunc::class,
 ];
 
+// Additional metadata labels. The override is registered for the default
+// language file only - TYPO3 picks up the de. sibling next to it on its own -
+// and its labels are merged into dlf's, so only new or changed ones are needed.
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:dlf/Resources/Private/Language/locallang_metadata.xlf'][]
+    = 'EXT:presentation_package/Resources/Private/Extensions/Dlf/Language/locallang_metadata.xlf';
+
 // Image proxy with access restrictions, formerly an eID script inside dlf.
 // The eID key must not change: it is part of the file URLs in every published
 // METS file.
